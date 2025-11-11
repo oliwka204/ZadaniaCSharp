99132ef628228bbd3d9c43072bfa6d3bbf079fda
oliwka204 <olicytrynowa@gmail.com>
2025-11-11 10:10:12 +0100

    Add files via upload
    Zadanie 1 Lab1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Pobieramy wartości a, b ,c 
+        Console.WriteLine("Podaj a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        // Używamy klasy DeltaCalculator z innego pliku!
+        Delta.ObliczDelteOrazPierwiastki(a, b, c);
+    }
+}
